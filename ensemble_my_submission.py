de55021c28cb175c1b2a09e90235eb2e92242b9e
aguang1201 <uubs@yeah.net>
2019-02-24 15:39:22 +0800

    modify src for multi GPU

---
+++ ensemble_my_submission.py
@@ -0,0 +1,63 @@
+import csv
+import pandas as pd # not key to functionality of kernel
+from datetime import datetime
+
+# sub_files = [
+#              '../input/weighted-average-ensemble-v-n-lb-0-776/sub_ns.csv',
+#              '../input/ensembling-voting-lb-0-777/sub_ens.csv',
+#              '../input/ensembling-voting-lb-0-777/sub_ens.csv'
+#             ]
+sub_files = [
+             'submit/submission_2019-01-30 09:31:30.242912.csv',
+             'submit/submission_2019-01-31 13:35:49.938504.csv',
+             'submit/submission_2019-02-20 11:54:59.539969.csv',
+             'submit/submission_2019-02-20 13:21:32.233588.csv'
+            ]
+
+# Weights of the individual subs
+sub_weight = [
+              0.904**2,
+              0.911**2,
+              0.910**2,
+              0.907**2
+             ]
+
+abc = pd.read_csv(sub_files[0])
+xyz = pd.read_csv(sub_files[1])
+
+print(abc.head())
+print(xyz.head())
+
+Hlabel = 'Image'
+Htarget = 'Id'
+npt = 5  # number of places in target
+
+place_weights = {}
+for i in range(npt):
+    place_weights[i] = (1 / (i + 1))
+
+print(place_weights)
+
+lg = len(sub_files)
+sub = [None] * lg
+for i, file in enumerate(sub_files):
+    ## input files ##
+    print("Reading {}: w={} - {}".format(i, sub_weight[i], file))
+    reader = csv.DictReader(open(file, "r"))
+    sub[i] = sorted(reader, key=lambda d: str(d[Hlabel]))
+
+## output file ##
+time_now = datetime.now()
+out = open(f"submit/sub_ens_{time_now}.csv", "w", newline='')
+writer = csv.writer(out)
+writer.writerow([Hlabel, Htarget])
+
+for p, row in enumerate(sub[0]):
+    target_weight = {}
+    for s in range(lg):
+        row1 = sub[s][p]
+        for ind, trgt in enumerate(row1[Htarget].split(' ')):
+            target_weight[trgt] = target_weight.get(trgt, 0) + (place_weights[ind] * sub_weight[s])
+    tops_trgt = sorted(target_weight, key=target_weight.get, reverse=True)[:npt]
+    writer.writerow([row1[Hlabel], " ".join(tops_trgt)])
+out.close()
